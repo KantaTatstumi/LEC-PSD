83edfaad941fae1a9c3c71c8eb7fc56f16e0e9e0
KantaTatstumi <tatsumikanta@gmail.com>
2023-06-30 21:22:52 +0700

    -

---
+++ LEC PSD/Views/Navbar.Master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LEC_PSD.Views
+{
+    public partial class Navbar : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Search_Button_Click(object sender, ImageClickEventArgs e)
+        {
+
+        }
+    }
+}
